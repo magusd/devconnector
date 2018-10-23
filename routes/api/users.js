332e53a12f08be8698fd32c2ad24aac0e320a6d5
Vitor Lobao <vitor.lobao@madeiramadeira.com.br>
2018-10-22 23:26:48 -0300

    user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../../models/User");
 
@@ -40,6 +41,27 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route GET api/users/loing
+// @desc Login User/Returns JWT Token
+// @access public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }).then(user => {
+    //Check user exists
+    if (!user) {
+      return res.status(404).json({ email: "User not found." });
+    }
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        return res.json({ message: "Success" });
+      }
+      return res.status(404).json({ password: "Password incorrect." });
+    });
   });
 });
 
